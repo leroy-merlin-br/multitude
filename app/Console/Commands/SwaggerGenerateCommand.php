045e503941111ed512a1094e2f54dc21c54e25c6
Zizaco Zizuini <zizaco@gmail.com>
2016-08-09 23:56:14 -0300

    Added swagger:generate command to generate swagger.json

---
+++ app/Console/Commands/SwaggerGenerateCommand.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Swagger;
+
+class SwaggerGenerateCommand extends Command
+{
+    /**
+     * Command Name.
+     * @var string
+     */
+    protected $name = 'swagger:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generates swagger.json based in project annotations';
+
+    /**
+     * Injects
+     * @param TinkerShell $shell [description]
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     *  Performs the event.
+     */
+    public function fire()
+    {
+        $paths = [
+            app('path'),
+            app('path').'/../Leadgen',
+        ];
+
+        $output = Swagger\scan($paths);
+
+        file_put_contents(app('path').'/../public/swagger.json', $output);
+
+        $this->info('swager.json generated successfully');
+    }
+}
