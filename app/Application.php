65dffdb65a86e6236e62180f147c2622abb5ee2c
Zizaco Zizuini <zizaco@gmail.com>
2016-08-13 20:33:00 -0300

    Extended `Lumen\Application` in order to customize which service providers are registered by default. Disabled sql migration service provider

---
+++ app/Application.php
@@ -0,0 +1,25 @@
+<?php
+namespace App;
+
+use Laravel\Lumen\Application as LumenApplication;
+
+class Application extends LumenApplication
+{
+    /**
+     * Prepare the application to execute a console command.
+     *
+     * @return void
+     */
+    public function prepareForConsoleCommand()
+    {
+        $this->withFacades();
+
+        $this->make('cache');
+        $this->make('queue');
+
+        $this->configure('database');
+
+        $this->register('Illuminate\Database\SeedServiceProvider');
+        $this->register('Illuminate\Queue\ConsoleServiceProvider');
+    }
+}
