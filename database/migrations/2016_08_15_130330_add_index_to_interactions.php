5799d003e4ecc87a2d7c610d37d5fb8fc137d1f6
Zizaco Zizuini <zizaco@gmail.com>
2016-08-15 10:41:56 -0300

    Added migrations to create indexes in `interactions` and `interactionTypes`

---
+++ database/migrations/2016_08_15_130330_add_index_to_interactions.php
@@ -0,0 +1,33 @@
+<?php
+
+use MongoDB\Database;
+
+/**
+ * Base MongoDB migration
+ */
+class AddIndexToInteractions
+{
+    /**
+     * Run the migrations
+     *
+     * @param  Database $db MongoDB Database
+     *
+     * @return void
+     */
+    public function up(Database $db)
+    {
+        $db->interactions->createIndex(['acknowledged' => 1]);
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @param  Database $db MongoDB Database
+     *
+     * @return void
+     */
+    public function down(Database $db)
+    {
+        $db->interactions->dropIndex('acknowledged');
+    }
+}
---
+++ database/migrations/2016_08_15_133227_add_index_to_interactiontypes.php
@@ -0,0 +1,33 @@
+<?php
+
+use MongoDB\Database;
+
+/**
+ * Base MongoDB migration
+ */
+class AddIndexToInteractiontypes
+{
+    /**
+     * Run the migrations
+     *
+     * @param  Database $db MongoDB Database
+     *
+     * @return void
+     */
+    public function up(Database $db)
+    {
+        $db->interactionTypes->createIndex(['slug' => 1]);
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @param  Database $db MongoDB Database
+     *
+     * @return void
+     */
+    public function down(Database $db)
+    {
+        $db->interactionTypes->dropIndex('slug');
+    }
+}
