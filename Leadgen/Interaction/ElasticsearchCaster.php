8fe550c93ad5718478f1ad21f2cc657cbc615871
Zizaco Zizuini <zizaco@gmail.com>
2016-09-05 22:07:13 -0300

    Added an utility class to cast Interaction into elasticsearch format

---
+++ Leadgen/Interaction/ElasticsearchCaster.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Leadgen\Interaction;
+
+class ElasticsearchCaster
+{
+    /**
+     * Cast an Interaction object to an associative array ready to be indexed
+     * in Elasticsearch.
+     *
+     * @param  Interaction $interaction Interaction to be casted.
+     *
+     * @return array Resulting associative array of Interaction.
+     */
+    static public function castToEs(Interaction $interaction)
+    {
+        $document = array_diff_key($interaction->attributes, ['_id' => 1, 'params' => 1]);
+
+        foreach(['created_at', 'updated_at'] as $dateField) {
+            $document[$dateField] = $interaction
+                ->$dateField
+                ->toDateTime()
+                ->format('Y-m-d\Th:i');
+        }
+
+        $document['params'] = [];
+
+        foreach ($interaction->params as $key => $value) {
+            if (is_numeric($value)) {
+                $document['params']["params/$key/float"] = $value;
+            }
+            $document['params']["params/$key/string"] = $value;
+        }
+
+        return $document;
+    }
+}
