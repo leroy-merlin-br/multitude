b56ed34ddba82826cead9ea42b08fc53abc9b0e4
Zizaco Zizuini <zizaco@gmail.com>
2016-12-22 23:08:53 -0200

    Updated `PreviewQuery` JS module in order to read the endpoint from the markup

--- resources/assets/js/preview-query.js
+++ resources/assets/js/preview-query.js
@@ -4,6 +4,7 @@ PreviewQuery.prototype.init = function ($el) {
   this.$el = $el;
   this.$queryBuilder = $($el.data('querybuilder'));
   this.$previewbox = $($el.data('previewbox'));
+  this.endpoint = $el.data('endpoint');
 
   this.registerEvents();
 }
@@ -11,15 +12,16 @@ PreviewQuery.prototype.init = function ($el) {
 PreviewQuery.prototype.registerEvents = function () {
   _this = this;
 
-  this.$el.click(function() {
+  this.$el.click(function(event) {
     var rules = _this.$queryBuilder.queryBuilder('getRules');
 
     _this.performRequest(rules);
+    event.preventDefault();
   })
 }
 
 PreviewQuery.prototype.performRequest = function (rules) {
-  var url = 'api/v1/customer?query=' + JSON.stringify(rules);
+  var url = this.endpoint + '?query=' + JSON.stringify(rules);
 
   // $.ajax({
   //   url: url,
