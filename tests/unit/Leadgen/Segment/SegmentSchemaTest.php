91a49f21302454737d8bd6a425624416936e8999
Zizaco Zizuini <zizaco@gmail.com>
2016-12-24 11:41:30 -0200

    Updated test cases of SegmentSchema cron values.

--- tests/unit/Leadgen/Segment/SegmentSchemaTest.php
+++ tests/unit/Leadgen/Segment/SegmentSchemaTest.php
@@ -26,11 +26,13 @@ class SegmentSchemaTest extends PHPUnit_Framework_TestCase
 
     public function cronFieldDataProvider()
     {
+        $default = '0 0 * * * *';
+
         return [
             // $in, $out
-            ['potato', null],
-            [123456, null],
-            ['* * *', null],
+            ['potato', $default],
+            [123456, $default],
+            ['* * *', $default],
             ['* * * * * *', '* * * * * *'],
             [' * * * * * *   ', '* * * * * *'],
             ['   * * 1 * 2 *', '* * 1 * 2 *'],
