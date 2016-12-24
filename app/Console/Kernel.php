ce3e9fa34421a684ad7b4625e0d6fddd80cd8d14
Zizaco Zizuini <zizaco@gmail.com>
2016-12-24 11:05:09 -0200

    Updated `Console\Kernel` to automatically schedulle the `leadgen:segment-update` for each existing segment. Now, just by creating a new segment it will automatically look for customers mathing it's rules and update then automatically :smile:

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,9 +7,14 @@ use App\Console\Commands\MigrationCommand;
 use App\Console\Commands\ProcessInteractionsCommand;
 use App\Console\Commands\SearchIndexCommand;
 use App\Console\Commands\SwaggerGenerateCommand;
+use App\Console\Commands\UpdateSegmentsCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
+use Leadgen\Segment\Repository;
 
+/**
+ * Register and schedule artisan commands.
+ */
 class Kernel extends ConsoleKernel
 {
     /**
@@ -20,15 +25,22 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         SwaggerGenerateCommand::class,
         ProcessInteractionsCommand::class,
+        UpdateSegmentsCommand::class,
         MigrationCommand::class,
         MakeMigrationCommand::class,
         SearchIndexCommand::class,
     ];
 
     /**
+     * Caches the segments that are being used to register commands
+     * @var \Mongolid\Cursor\CursorInterface
+     */
+    protected $segments;
+
+    /**
      * Define the application's command schedule.
      *
-     * @param \Illuminate\Console\Scheduling\Schedule $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule Lumen scheduler.
      *
      * @return void
      */
@@ -37,5 +49,58 @@ class Kernel extends ConsoleKernel
         $schedule->command('leadgen:proc-interaction')
             ->everyMinute()
             ->withoutOverlapping();
+
+        // Schedules every Segment to be updated
+        $this->scheduleMany($schedule, $this->getCommandsToSchedule());
+    }
+
+    /**
+     * Schedules many commands.
+     *
+     * @param  Schedule $schedule Lumen scheduler.
+     * @param  array    $commands Array where the key is the command being scheduled and the value is a cron string.
+     *
+     * @return void
+     */
+    protected function scheduleMany(Schedule $schedule, array $commands)
+    {
+        foreach ($commands as $commandToRun => $cron) {
+            if ($cron) {
+                $schedule->command($commandToRun)
+                    ->cron($cron)
+                    ->withoutOverlapping();
+            }
+        }
+    }
+
+    /**
+     * Returns an associative array where each command has a cron value of
+     * when it should be executed
+     *
+     * @return array
+     */
+    protected function getCommandsToSchedule()
+    {
+        $commands = [];
+
+        foreach ($this->getSegments() as $segment) {
+            $commands["leadgen:segment-update --add $segment->slug"] = $segment->additionInterval;
+            // $commands["leadgen:segment-update --remove $segment->slug"] = $segment->removalInterval;
+        }
+
+        return $commands;
+    }
+
+    /**
+     * Returns a cursor containing all segments
+     * @return \Mongolid\Cursor\CursorInterface
+     */
+    protected function getSegments()
+    {
+        if (! $this->segments) {
+            $this->segments = app()->make(Repository::class)->all(1, 300);
+        }
+
+        return $this->segments;
     }
 }
