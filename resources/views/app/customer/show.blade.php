4044c455dab0350c9fa6e6b2f5e9d8ee87d180ac
Zizaco Zizuini <zizaco@gmail.com>
2016-12-25 23:09:51 -0200

    Updated `app.customer.show` view in order to better display channels and the location of the customer.

--- resources/views/app/customer/show.blade.php
+++ resources/views/app/customer/show.blade.php
@@ -14,23 +14,40 @@
         <div class="col-xs-12">
             <h3 class="link">{{ $customer->email ?: $customer->docNumber }}</h3>
             <img data-module="Gravatar" data-email="{{ $customer->email }}" class="circular">
+            <p>
+                <strong>Location</strong>
+                <span class="badge">{{ $customer->location ?: "none" }}</span>
+            </p>
 
-            <table class="table table-zebra">
+            <table class="table table-zebra table-bordered">
                 <thead>
                     <tr>
                         <th>When</th>
-                        <th></th>
+                        <th>Channel</th>
                         <th>Interaction</th>
                         <th>Details</th>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach($customer->interactions() as $interaction)
+                    @foreach($customer->interactions()->sort(['_id' => -1]) as $interaction)
                     <tr>
                         <td>{{ $interaction->created_at->toDateTime()->format('Y-m-d H:i') }}</td>
-                        <th><div class="glyph glyph-magnifier"></div></th>
-                        <td>{{ $interaction->interaction }}</td>
-                        <td><code>{{ json_encode($interaction->params) }}</code></td>
+                        <th>
+                            @if ('mobile' == $interaction->channel)
+                                <img title="mobile" width="32" src="http://image.flaticon.com/icons/svg/15/15874.svg">
+                            @else
+                                <img title="web" width="32" src="http://image.flaticon.com/icons/svg/34/34288.svg">
+                            @endif
+                        </th>
+                        <td><span class="badge badge-primary">{{ $interaction->interaction }}</span></td>
+                        <td><code>
+                            @foreach($interaction->params as $key => $value)
+                                <p>
+                                    <strong>{{ $key }}:</strong>
+                                    {!! is_array($value) ? implode(',<br>', $value) : $value !!}
+                                </p>
+                            @endforeach
+                        </code></td>
                     </tr>
                     @endforeach
                 </tbody>
