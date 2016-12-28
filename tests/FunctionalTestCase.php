c7cac8e00199feca5a0777aa893fe583e2c3a865
Zizaco Zizuini <zizaco@gmail.com>
2016-12-28 16:15:12 -0200

    Improved TestCases to support more complex functional test scenarios.

--- tests/FunctionalTestCase.php
+++ tests/FunctionalTestCase.php
@@ -1,7 +1,16 @@
 <?php
 
-class FunctionalTestCase extends Laravel\Lumen\Testing\TestCase
+use App\Console\Kernel;
+
+class FunctionalTestCase extends TestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->runCommand('db:searchindex');
+    }
+
     /**
      * Creates the application.
      *
@@ -29,5 +38,36 @@ class FunctionalTestCase extends Laravel\Lumen\Testing\TestCase
         );
 
         return $this;
+    }
+
+    /**
+     * Runs an artisan command by it's name
+     *
+     * @param  string $command Command name
+     * @param  array  $params  Command params
+     *
+     * @return FunctionalTestCase  Self
+     */
+    protected function runCommand($command, $params = [])
+    {
+        $this->app->make(Kernel::class)->call($command, $params);
+
+        return $this;
+    }
+
+    /**
+     * Calls the Refresh API of Elasticsearch. Making indexed documents ready
+     * to be queryied.
+     *
+     * @see https://www.elastic.co/guide/en/elasticsearch/reference/2.3/indices-refresh.html
+     */
+    protected function waitElasticsearchOperations()
+    {
+        $indiceName = $this->app->make('config')->get('elasticsearch.defaultIndex');
+
+        $this->app->make(\Elasticsearch\Client::class)
+            ->indices()->refresh(['index' => $indiceName]);
+
+        usleep(2 * 1000);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,4 +11,27 @@ class TestCase extends Laravel\Lumen\Testing\TestCase
     {
         return require __DIR__.'/../bootstrap/app.php';
     }
+
+    /**
+     * Set a protected property of an object
+     *
+     * @param  mixed  $obj      Object Instance.
+     * @param  string $property Property name.
+     * @param  mixed  $value    Value to be set.
+     *
+     * @return void
+     */
+    protected function setProtected($obj, $property, $value)
+    {
+        $class = new ReflectionClass($obj);
+        $property = $class->getProperty($property);
+        $property->setAccessible(true);
+
+        if (is_string($obj)) { // static
+            $property->setValue($value);
+            return;
+        }
+
+        $property->setValue($obj, $value);
+    }
 }
