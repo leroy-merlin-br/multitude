e44677a83b55aae450fc1adb57dd1b9c225bec40
Zizaco Zizuini <zizaco@gmail.com>
2016-12-29 09:30:46 -0200

    Added ExactTarget trigger entry point. `Leadgen\ExactTarget\ExactTarget` should be the class referenced by the `\Trigger` 'type' of a Segment in order to fire it.

---
+++ Leadgen/ExactTarget/ExactTargetTrigger.php
@@ -0,0 +1,37 @@
+<?php
+namespace Leadgen\ExactTarget;
+
+/**
+ * ExactTarget Trigger to be Ignited from a Trigger entity
+ */
+class ExactTargetTrigger
+{
+    /**
+     * @var CustomerUpdater
+     */
+    protected $customerUpdater;
+
+    /**
+     * Injects dependencies
+     * @param CustomerUpdater $customerUpdater Customer updater instance.
+     */
+    public function __construct(CustomerUpdater $customerUpdater)
+    {
+        $this->customerUpdater = $customerUpdater;
+    }
+
+    /**
+     * Trigger entry point
+     *
+     * @param  mixed $customers Array or iterator of Customer objects.
+     * @param  array $settings  Associative array of settings.
+     *
+     * @throws \InvalidArgumentException If the $resources is not an array of iterable.
+     *
+     * @return mixed Results
+     */
+    public function fireTrigger($customers, array $settings)
+    {
+        return $this->customerUpdater->send($customers, $settings['dataExtension'] ?? 'multitudeCustomers');
+    }
+}
